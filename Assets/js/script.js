fe6b16e670078e84126120c0cfb3d794a638da74
Colin Robinson <colindrums@me.com>
2024-03-28 11:48:19 -0400

    trying out a few things in javascript

---
+++ Assets/js/script.js
@@ -0,0 +1,44 @@
+// Access toggle switch HTML element
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('.container');
+
+// Set default mode to dark
+let mode = 'dark';
+
+// Listen for a click event on toggle switch
+themeSwitcher.addEventListener('click', function () {
+  // If mode is dark, apply light background
+  if (mode === 'dark') {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+  }
+  // If mode is light, apply dark background
+  else {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+  }
+});
+
+const userName = document.getElementById('user');
+const title  = document.getElementById('title');
+const comment = document.getElementById('msg');
+// const submitButton = document.getElementById('submit-comment-btn');
+
+const form = document.querySelector('#comment-form');
+const commentBox = document.querySelector('#comment-box');
+const submitButton = document.querySelector('#submit-comment-btn');
+
+// Create a function to save the comment to local storage
+const saveCommentToLocalStorage = (comment) => {
+  const comments = JSON.parse(localStorage.getItem('comments')) || [];
+  comments.push(comment);
+  localStorage.setItem('comments', JSON.stringify(comments));
+};
+
+// Add an event listener to the submit button
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const comment = commentBox.value;
+  saveCommentToLocalStorage(comment);
+  commentBox.value = '';
+});
